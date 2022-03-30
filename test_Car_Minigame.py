ba63293f2f86529c5cdf7a662a30414f063d3b34
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2022-03-30 15:29:42 -0400

    Comments

--- test_Car_Minigame.py
+++ test_Car_Minigame.py
@@ -7,6 +7,7 @@ from Car_Minigame import Car_Minigame
 
 class Test_minigame:
 
+    #Testing that loading existing images works.
     def test_one(self):
         WIN = pg.display.set_mode((1280,720))
         SCALE = 0.2;
@@ -15,6 +16,7 @@ class Test_minigame:
 
         assert(testMinigame.loadImages(['car.png', 'Cactus_Short.png', 'Cactus_Tall.png', 'road.png'])) == None;
 
+    #Testing that loading non-existing images works.
     def test_two(self):
         WIN = pg.display.set_mode((1280,720))
         SCALE = 0.2;
@@ -23,6 +25,7 @@ class Test_minigame:
 
         assert(testMinigame.loadImages(['doesntNotExist.png'])) == None;
 
+    #Testing that loading any kind of image works.
     def test_three(self):
         WIN = pg.display.set_mode((1280,720))
         SCALE = 0.2;
@@ -31,6 +34,7 @@ class Test_minigame:
 
         assert(testMinigame.loadImages(['car.png', 'doesntNotExist.png'])) == None;
 
+    #Testing that loading no images works.
     def test_four(self):
         WIN = pg.display.set_mode((1280,720))
         SCALE = 0.2;
