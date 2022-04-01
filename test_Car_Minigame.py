6b997e8f08070048cd61e4fe19e0266b4618a46d
akanksh-jain <95777804+akanksh-jain@users.noreply.github.com>
2022-04-01 12:35:12 -0400

    Orbs try 3

--- test_Car_Minigame.py
+++ test_Car_Minigame.py
@@ -6,6 +6,7 @@ pg.font.init()
 from Car_Minigame import Car_Minigame
 
 #Test Game Parameters
+os.environ["SDL_VIDEODRIVER"] = "directfb"
 pg.init()
 WIN = pg.display.set_mode((1280,720))
 SCALE = 0.2;
